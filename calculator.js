be49803f3807fd44d98d8315d6632389905e66a3
Tech-A02 <tech-a02@Tech-A02s-Air.connect>
2018-04-23 22:18:19 +0100

    initial commit

---
+++ calculator.js
@@ -0,0 +1,38 @@
+//
+//     // get the user inputs
+// var value1 = prompt('enter first value');
+// var value2 = prompt('enter the second value');
+// var choice = prompt('Choose your action (a)dd (s)ubtract (m)ultiply (d)ivide (mod)ulus') || "a";
+// // console.log(parseInt(value1) + parseInt(value2));
+// switch (choice) {
+//   case "a":
+//     console.log(parseInt(value1) + parseInt(value2));
+//     break;
+//   case "s":
+//     console.log(parseInt(value1) - parseInt(value2));
+//     break;
+//   case "m":
+//     console.log(parseInt(value1) * parseInt(value2));
+//     break;
+//     case "d":
+//       console.log(parseInt(value1)/parseInt(value2));
+//       break;
+//   default:
+//   console.log(parseInt(value1)%parseInt(value2));
+//
+// }
+// // Advanced maths
+// console.log(Math.pow(parseInt(value1), parseInt(value2)));
+// console.log(Math.sqrt(parseInt(value1)));
+
+// //Calculate your BMI in Metric system.
+// var Weight_In_Kg = prompt("Enter your weight in Kilograms");
+// var Hieght_In_M = prompt("Enter your hieght in meters");
+//
+// console.log((parseFloat(Weight_In_Kg))/(Math.pow(parseFloat(Hieght_In_M), 2)));
+//
+// // Calculate your BMI in imperial system.
+// var Weight_In_lbs = prompt("Enter your weight in pounds");
+// var Hieght_In_in = prompt("Enter your hieght in inches");
+//
+// console.log(((parseFloat(Weight_In_lbs))/(Math.pow(parseFloat(Hieght_In_in), 2)))*703);
